93e10b11135d4b42316486996ceedf91b28d2d68
bajosoto <sergio.soto@live.com>
2016-05-12 10:47:36 -0400

    Create urlify.c

---
+++ Chapter 1 - Arrays and Strings/urlify.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int countSpaces(char *str);
+char *urlify(char *str);
+char *createHelper(char *str);
+
+char *createHelper(char *str){
+ 	int spaceCnt = countSpaces(str);
+ 	int length = strlen(str);
+ 	char *bigStr = (char *)malloc(sizeof(char) * 
+                                (length + 2 * spaceCnt) );
+ 	return bigStr;
+}
+
+int countSpaces(char *str){
+ 	int spaceCnt = 0;
+ 	int i;
+ 	for(i = 0; i < lenght; i++){
+ 		if(str[i] == ' ')
+ 			spaceCnt++;
+ 	} 
+ 	return spaceCnt;
+}
+
+char *urlify(char *str){
+ 	char *longStr = createHelper(str);
+ 	int i;
+ 	int lenght = strlen(str);
+ 	int spaceCnt = 0;
+ 	if(str == NULL){
+		return NULL;
+ 	}else{
+ 		for(i = 0; i < length; i++)
+		{
+ 			if(str[i] != ' '){
+ 				longStr[i + spaceCnt * 2] = str[i];
+ 			}else{ 
+ 				longStr[i + spaceCnt * 2] = '%';
+ 				longStr[i + spaceCnt * 2 + 1] = '2';
+ 				longStr[i + spaceCnt * 2 + 2] = '0';
+ 				spaceCnt++;
+ 			}
+ 		}
+		return longStr; 
+ 	}
+}
+
+int main(int argc, argv[]){
+  	char *myStr;
+ 	if(argc < 1){
+ 		return -1;
+ 	}else{
+ 		myStr = urlify(argv[1])
+ 		printf("Original: %s\n", str);
+		printf("URLified: %s\n", myStr);
+ 		free(myStr);
+ 	}
+ 	return 0;
+}
